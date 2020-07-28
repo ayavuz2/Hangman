de30b49150ef8ece459563428cf16b4f69fcd6bc
ayavuz2 <a.yavuzum@gmail.com>
2020-07-28 19:05:29 +0300

    Setting up basic stuff

--- hangman.py
+++ hangman.py
@@ -1,12 +1,26 @@
 import pygame
 import os
 
+# setup display
 pygame.init()
 
 WIDTH, HEIGHT = 800, 500
-pygame.display.set_mode((WIDTH, HEIGHT))
+win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman Game")
 
+# load images
+images = []
+for i in range(7):
+	image = pygame.image.load("hangman{}.png".format(i))
+	images.append(image)
+
+# game variables
+hangman_status = 0
+
+# colors
+WHITE = (255,255,255)
+
+# setup game loop
 FPS = 60
 clock = pygame.time.Clock()
 run = True
@@ -14,10 +28,16 @@ run = True
 while run:
 	clock.tick(FPS)
 
+	win.fill(WHITE)
+	win.blit(images[hangman_status], (150, 100)) # blit(): Draw image/surface
+	pygame.display.update()
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
-
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			pos = pygame.mouse.get_pos()
+			print(pos)
 
 
 pygame.quit()
